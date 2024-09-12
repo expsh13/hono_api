d45046f58059254dab5a61d5b8b2b54648a1d16f
Satoki Hata <satokihata@gmail.com>
2024-09-12 21:02:00 +0900

    add 認証

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
 import posts from "./blogs/blogs";
+import auth from "./auth/auth";
+import { basicAuth } from "hono/basic-auth";
 
 const app = new Hono();
 
@@ -10,6 +12,16 @@ app.get("/", (c) => {
 
 // ミドルウェア
 app.use("*", prettyJSON());
-app.route("/post", posts);
+app.use(
+  "/auth/*",
+  basicAuth({
+    username: "hono",
+    password: "acoolproject",
+  })
+);
+
+app.route("/auth", auth);
+
+app.route("/posts", posts);
 
 export default app;
